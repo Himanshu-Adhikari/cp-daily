99288ebd1842669a1082a2be4287f635064db229
Himanshu-Adhikari <adhikarihimanshu393@gmail.com>
2026-01-31 11:34:42 +0530

    2026-01-31

---
+++ solutions/2026-01-31/gfg_implement-k-queues-in-a-single-array.cpp
@@ -0,0 +1,30 @@
+class kQueues {
+
+  public:
+    int sz;
+    int k,curr;
+    map<int,queue<int>>m;
+    kQueues(int n, int k) {
+        sz=n;
+        this->k=k;
+        curr=0;
+    }
+    void enqueue(int x, int i) {
+        if(curr<sz){m[i].push(x);curr+=1;}
+    }
+
+    int dequeue(int i) {
+        if(m[i].empty())return -1;
+        int res=m[i].front();m[i].pop();
+        curr-=1;
+        return res;
+    }
+
+    bool isEmpty(int i) {
+        return curr==0;
+    }
+
+    bool isFull() {
+        return curr==sz;
+    }
+};
